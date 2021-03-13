82477204c675d78d60b6c43b678dd1bc69ed2f21
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-13 17:13:11 -0500

    Updating files

---
+++ d8s_ip_addresses/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version('democritus_ip_addresses')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "democritus_ip_addresses". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-ip-addresses/issues.'
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+
+from .ip_addresses import *
