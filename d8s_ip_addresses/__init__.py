2489c9c7c4056826a9af7a834bcf67b951a9d164
Floyd Hightower <floyd.hightower27@gmail.com>
2021-03-17 07:58:44 -0500

    Adding support for bumpversion
        Fixes https://github.com/democritus-project/roadmap/issues/69

--- d8s_ip_addresses/__init__.py
+++ d8s_ip_addresses/__init__.py
@@ -1,20 +1,5 @@
-try:
-    from importlib.metadata import PackageNotFoundError, version
-except ImportError:
-    from importlib_metadata import PackageNotFoundError, version  # type: ignore
-
 from .ip_addresses import *
 
-try:
-    __version__ = version('d8s_ip_addresses')
-except PackageNotFoundError:
-    message = (
-        'Unable to find a version number for "d8s_ip_addresses". '
-        + 'This likely means the library was not installed properly. '
-        + 'Please re-install it and, if the problem persists, '
-        + 'raise an issue here: https://github.com/democritus-project/democritus-ip-addresses/issues.'
-    )
-    print(message)
-
+__version__ = '0.2.0'
 __author__ = '''Floyd Hightower'''
 __email__ = 'floyd.hightower27@gmail.com'
